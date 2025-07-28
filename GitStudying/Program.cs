9478199bc19e9544d21a0cf239de2560b5ea48a1
Maria_Mouradian <mariamouradian@yandex.ru>
2025-07-29 00:33:16 +0300

    Initial commit local

---
+++ GitStudying/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GitStudying
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //Some changes
+        }
+    }
+}
